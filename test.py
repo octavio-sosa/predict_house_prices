9d6c1d3b634dcdd4ac8234a81d6e7c4987c24c35
octavio-sosa <octavio@ku.edu>
2020-11-16 13:53:47 -0600

    test data

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
 import numpy as np
 import nn as nn
+import data_processor as dp
 
-def test():
+
+def weights():
     data = np.full((3,3), 2)
     s = nn.sigmoid(data)
     weights = nn.init_weights(data.shape[1], data.shape[1])
     print(weights)
+
+def data():
+    X_train, X_test, Y_train, Y_test = dp.get_data()
+    data = {'X_train': X_train, 'X_test': X_test, 'Y_train': Y_train, 'Y_test': Y_test}
+    for key in data.keys():
+        print(f'{key}: {data[key].shape}')
+
+def forward():
+    X_train, X_test, Y_train, Y_test = dp.get_data()
+    
+
+def test():
+    data()
+    #forward()
 
 test()
